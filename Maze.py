8f12663fa988a858e4758218f84a9e4defec5761
karma yonten thaye@student.curtincollege.edu.au <karmayontenthaye@Karmas-MacBook-Pro.local>
2022-03-19 14:02:44 +0800

    second commit

--- Maze.py
+++ Maze.py
@@ -1,3 +1,5 @@
+import Gridviewer
+
 EMPTY = 0
 WALL = 1
 START = 2
@@ -20,31 +22,8 @@ if __name__ == "__main__":
         [ WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END],
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
-                    
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
 
-
+  Gridviewer.view(grid)              
     print("Find a solution to get from ^^ to $$, using the characters " 
         + "'" + NORTH + "', '" + EAST + "', '" + SOUTH + "' and '" + WEST + "'"
         + " (for north, east, south and west).")
@@ -108,26 +87,4 @@ if __name__ == "__main__":
         print("MESSAGE 5") # Did not reach the end.
 
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
-
+ Gridviewer.view(grid)
