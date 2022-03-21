01a0332e3694769cef4508fc3a5e31280449326b
KarmaYonten <101979008+KarmaYonten@users.noreply.github.com>
2022-03-21 21:20:18 +0800

    had to clone manually

--- lec4/Maze.py
+++ lec4/Maze.py
@@ -5,7 +5,7 @@ WALL = 1
 START = 2
 END = 3
 VISITED = 4
-    
+
 NORTH = 'n'
 EAST = 'e'
 SOUTH = 's'
@@ -14,77 +14,81 @@ WEST = 'w'
 if __name__ == "__main__":
     grid = [
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
-        [START, EMPTY,  WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL],
-        [ WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL],
+        [START, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL],
+        [ WALL, WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL],
         [ WALL,  WALL,  WALL,  WALL, EMPTY,  WALL, EMPTY,  WALL, EMPTY, WALL],
-        [ WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, WALL],
-        [ WALL,  WALL, EMPTY,  WALL,  WALL, EMPTY, EMPTY,  WALL, EMPTY, WALL],
-        [ WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END],
+        [ WALL, EMPTY, WALL, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, WALL],
+        [ WALL,  WALL, WALL,  WALL,  WALL, EMPTY, EMPTY,  WALL, EMPTY, WALL],
+        [ WALL,  WALL, EMPTY, WALL, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END],
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
-     Gridviewer.view(grid) 
-         
-    print("Find a solution to get from ^^ to $$, using the characters " 
+
+  Gridviewer.view(grid)
+    print("Find a solution to get from ^^ to $$, using the characters "
         + "'" + NORTH + "', '" + EAST + "', '" + SOUTH + "' and '" + WEST + "'"
         + " (for north, east, south and west).")
     solution = input("Your solution: ")
 
-    currentRow = 1
-    currentCol = 0
+    Row = 1
+    Col = 0
     done = False
     solved = False
     charIndex = 0
     solutionLength = len(solution)
 
     while not done and charIndex < solutionLength:
-        
-        direction = solution[charIndex]
-        print("Location: (" + str(currentRow) + ", " + str(currentCol) 
-            + "), next direction: '" + direction + "'")
-        
-        if direction == NORTH:
-            currentRow -= 1
-            
-        elif direction == EAST:
-            currentCol += 1
-                
-        elif direction == SOUTH:
-            currentRow += 1
-                
-        elif direction == WEST:
-            currentCol -= 1
-        
-        else:
-            print("MESSAGE 1") # Invalid direction.
-        
-        if (currentRow < 0 or currentCol < 0 
-                        or currentRow >= len(grid) 
-                        or currentCol >= len(grid[currentRow])):
-            done = True
-            print("MESSAGE 2") # Out of bounds.
-            
-        else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
-                
-            elif grid[currentRow][currentCol] == WALL:
-                done = True
-                print("MESSAGE 3") # Hit wall.
 
-            elif grid[currentRow][currentCol] == END:
-                done = True
-                solved = True
-                print("MESSAGE 4") # Solved.
-                
-            else:
-                pass # Do nothing
-        
+        direction = solution[charIndex]
+        print("Location: (" + str(Row) + ", " + str(Col)
+            + "), next direction: '" + direction + "'")
+
+        if direction == NORTH:
+            Row -= 1
+
+        elif direction == EAST:
+            Col += 1
+
+        elif direction == SOUTH:
+            Row += 1
+
+        elif direction == WEST:
+            Col -= 1
+
+        else:
+            print("You have no idea where you're going") # Invalid direction.
+
+        if (Row < 0 or Col < 0
+                        or Row >= len(grid)
+                        or Col >= len(grid[Row])):
+            done = True
+            print("You fall into the chasm of doom.") # Out of bounds.
+
+        else:
+<<<<<<< HEAD
+             cell = grid[currentRow][currentCol]
+             if cell == EMPTY:
+                grid[currentRow][currentCol] = VISITED
+
+        elif cell == WALL:
+            done = True
+            print("You stumble blindly into a solid concrete wall.")
+
+        elif cell == END:
+             done = True
+             solved = True
+             print("SOLVED!")
+
+         else:
+                #Do nothing
+
         charIndex += 1
     # end-while
 
 
     if not solved:
-        print("MESSAGE 5") # Did not reach the end.
+        print("You have failed to escape. Future archeologists gaze upon your remains in bafflement") # Did not reach the end.
 
 
-        Gridviewer.view(grid)
+ Gridviewer.view(grid)
+
+
